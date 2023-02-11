6df2bee54b0d4cfae2a44685cad8b2424f3cdeee
Temirlan Zhangazy <61193731+temirlanzhangazy@users.noreply.github.com>
2023-02-12 01:08:17 +0600

    1.3.6 cors + json headers

--- api/checkVersion.php
+++ api/checkVersion.php
@@ -1,4 +1,9 @@
 <?php
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET');
+    header("Access-Control-Allow-Headers: X-Requested-With");
+    header('Content-Type: application/json; charset=utf-8');
+    
     $version = [136, 136];
     echo json_encode($version, JSON_UNESCAPED_UNICODE);
 ?>
